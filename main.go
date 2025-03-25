f90640db2f1b5dd97543bbf7a4778cb8a7d16756
swaran21 <saiswaran212005@gmail.com>
2025-03-25 23:44:10 +0530

    final-1

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 
 	// Configure CORS
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"https://snake-game-frontend-flgk.onrender.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
